12b9b6f168532198294e9ef371c1065b4af49839
bjanderson1 <bjanderson@wpi.edu>
2019-02-23 12:37:34 -0500

    Made q_learning function

--- group26/q_learning.py
+++ group26/q_learning.py
@@ -5,12 +5,25 @@ sys.path.insert(0, "../")
 
 from entity import *
 
+NUM_WEIGHTS = 6
+
 class QLearner:
     def __init__(self, weights):
         self.weights = weights
 
+    #returns (dx, dy, bomb?)
     def bestMove(self, world, character):
+
         pass
 
     def updateWeights(self, prevWorld, newWorld, character):
         pass
+
+    def q(self, world, character):
+        sum = 0;
+        sum += weights[0] / q_functions.f_to_exit(world, character) ** 2
+        sum += weights[1] / q_functions.f_to_monster(world, character) ** 2
+        sum += weights[2] / q_functions.f_to_bomb(world, character) ** 2
+        sum += weights[3] / q_functions.f_to_wall(world, character) ** 2
+        sum += weights[4] / q_functions.f_time_to_explosion(world, character) ** 2
+        sum += weights[5] * q_functions.f_is_exploded(world, character)
