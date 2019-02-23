cec33bbed4d678830c7382ac67919d7ae0638fa8
samgoldman <sgoldman216@gmail.com>
2019-02-23 13:21:25 -0500

    Added trainer for s1v1

---
+++ group26/scenario1/variant1_trainer.py
@@ -0,0 +1,51 @@
+# This is necessary to find the main code
+import sys
+sys.path.insert(0, '../../bomberman')
+sys.path.insert(1, '..')
+
+# Import necessary stuff
+from game import Game
+
+# TODO This is your code!
+sys.path.insert(1, '../group26')
+from q_character import qCharacter
+from q_learning import QLearner
+
+
+
+
+qLearner = QLearner([1, 1, 1, 1, 1, 1])
+
+
+for i in range(0, 1100):
+    # Create the game
+    g = Game.fromfile('map.txt', display=False)
+
+    # TODO Add your character
+    g.add_character(qCharacter("me", # name
+                                  "C",  # avatar
+                                  0, 0,  # position
+                               qLearner,
+                               True,
+                               i,
+                               1000))
+
+    # Run!
+    g.go()
+
+# Create the game
+g = Game.fromfile('map.txt', display=True)
+
+# TODO Add your character
+g.add_character(qCharacter("me",  # name
+                           "C",  # avatar
+                           0, 0,  # position
+                           qLearner,
+                           False,
+                           i,
+                           1000))
+
+# Run!
+g.go()
+
+print(qLearner.weights)
