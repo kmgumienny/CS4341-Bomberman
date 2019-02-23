c8d36b4af0a753114292ae09f44ddab27dc57c68
samgoldman <sgoldman216@gmail.com>
2019-02-23 13:26:53 -0500

    Fixing a number

--- group26/scenario1/variant1_trainer.py
+++ group26/scenario1/variant1_trainer.py
@@ -6,7 +6,6 @@ sys.path.insert(1, '..')
 # Import necessary stuff
 from game import Game
 
-# TODO This is your code!
 sys.path.insert(1, '../group26')
 from q_character import qCharacter
 from q_learning import QLearner
@@ -17,11 +16,10 @@ from q_learning import QLearner
 qLearner = QLearner([1, 1, 1, 1, 1, 1])
 
 
-for i in range(0, 1100):
+for i in range(0, 1000):
     # Create the game
     g = Game.fromfile('map.txt', display=False)
 
-    # TODO Add your character
     g.add_character(qCharacter("me", # name
                                   "C",  # avatar
                                   0, 0,  # position
@@ -36,7 +34,6 @@ for i in range(0, 1100):
 # Create the game
 g = Game.fromfile('map.txt', display=True)
 
-# TODO Add your character
 g.add_character(qCharacter("me",  # name
                            "C",  # avatar
                            0, 0,  # position
