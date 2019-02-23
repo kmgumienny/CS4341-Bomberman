a4026f8fe053d6b46fbd5588042c19052cfc2d7d
samgoldman <sgoldman216@gmail.com>
2019-02-23 13:02:21 -0500

    Updates to q_functions and q_learning

--- group26/q_functions.py
+++ group26/q_functions.py
@@ -3,7 +3,9 @@ from world_utilities import *
 
 
 # 1/(distance to closest exit)^2
-def f_to_exit(world, character_location):
+def f_to_exit(world, character):
+    character_location = (character.x, character.y)
+
     exits = find_exits(world)
 
     if len(exits) == 0:
@@ -17,7 +19,9 @@ def f_to_exit(world, character_location):
 
 
 # 1/(distance to closest monster)^2
-def f_to_monster(world, character_location):
+def f_to_monster(world, character):
+    character_location = (character.x, character.y)
+
     monsters = find_monsters(world)
 
     if len(monsters) == 0:
@@ -31,7 +35,9 @@ def f_to_monster(world, character_location):
 
 
 # 1/(distance to closest bomb)^2
-def f_to_bomb(world, character_location):
+def f_to_bomb(world, character):
+    character_location = (character.x, character.y)
+
     bombs = find_bombs(world)
 
     if len(bombs) == 0:
@@ -45,13 +51,15 @@ def f_to_bomb(world, character_location):
 
 
 # 1/(distance to closest wall)^2
-def f_to_wall(world, character_location):
+def f_to_wall(world, character):
+    character_location = (character.x, character.y)
+
     walls = find_walls(world)
 
     if len(walls) == 0:
         return 0
 
-    closest_wall = closest_point(character_location, walls, euclidean=False)
+    closest_exit = closest_point(character_location, walls, euclidean=False)
 
     a_star_distance = a_star(world, character_location, closest_wall)[1]
 
@@ -59,7 +67,9 @@ def f_to_wall(world, character_location):
 
 
 # 1/(time to explosion of closest bomb)^2
-def f_time_to_explosion(world, character_location):
+def f_time_to_explosion(world, character):
+    character_location = (character.x, character.y)
+
     bombs = find_bombs(world)
 
     if len(bombs) == 0:
@@ -73,7 +83,12 @@ def f_time_to_explosion(world, character_location):
 
 # return 1 if the cell the character is in will explode, or has exploded
 # return 0 if the cell the character is in will not explode
-def f_is_exploded(world, character_location):
+def f_is_exploded(world, character):
+    if world.me(character) is None:
+        return 1
+
+    character_location = (character.x, character.y)
+
     bombs = find_bombs(world)
 
     if len(bombs) == 0:
@@ -84,7 +99,7 @@ def f_is_exploded(world, character_location):
 
     world.add_blast(closest_bomb)
 
-    if world.explosion_at(character_location[0], character_location[1]) is not None:
+    if world.explosion_at(character.x, character.y) is not None:
         return 1
     else:
         return 0
--- group26/q_learning.py
+++ group26/q_learning.py
@@ -1,5 +1,5 @@
 import sys
-import q_functions
+from q_functions import *
 
 sys.path.insert(0, "../")
 
@@ -7,23 +7,52 @@ from entity import *
 
 NUM_WEIGHTS = 6
 
+from sensed_world import SensedWorld
+
+POSSIBLE_MOVES = [(1, 0, 0), (1, 1, 0), (1, -1, 0), (-1, 0, 0), (-1, 1, 0), (-1, -1, 0), (0, -1, 0), (0, 1, 0),
+                  (1, 0, 1), (1, 1, 1), (1, -1, 1), (-1, 0, 1), (-1, 1, 1), (-1, -1, 1), (0, -1, 1), (0, 1, 1)]
+
+GAMMA = .9
+ALPHA = .2
+
 class QLearner:
     def __init__(self, weights):
         self.weights = weights
 
     #returns (dx, dy, bomb?)
     def bestMove(self, world, character):
+        max_q = -9999999999
+        max_a = None
+        for move in POSSIBLE_MOVES:
+            new_world = SensedWorld.from_world(world)
+            new_world.characters[character.name].move(move[0], move[1])
+            if move[2] == 1:
+                new_world.characters[character.name].place_bomb()
 
-        pass
+            q = self.Q(new_world, new_world.characters[character.name])
+            if q > max_q:
+                max_q = q
+                max_a = move
 
-    def updateWeights(self, prevWorld, newWorld, character):
-        pass
+        return (max_a, max_q)
 
-    def q(self, world, character):
-        sum = 0;
-        sum += weights[0] / q_functions.f_to_exit(world, character) ** 2
-        sum += weights[1] / q_functions.f_to_monster(world, character) ** 2
-        sum += weights[2] / q_functions.f_to_bomb(world, character) ** 2
-        sum += weights[3] / q_functions.f_to_wall(world, character) ** 2
-        sum += weights[4] / q_functions.f_time_to_explosion(world, character) ** 2
-        sum += weights[5] * q_functions.f_is_exploded(world, character)
+    def updateWeights(self, prevWorld, newWorld, character, reward):
+        delta = (reward + GAMMA*self.bestMove(newWorld, character)[1]) - self.Q(newWorld, character)
+
+        self.weights[0] += ALPHA * delta * f_to_exit(prevWorld, character)
+        self.weights[1] += ALPHA * delta * f_to_monster(prevWorld, character)
+        self.weights[2] += ALPHA * delta * f_to_bomb(prevWorld, character)
+        self.weights[3] += ALPHA * delta * f_to_wall(prevWorld, character)
+        self.weights[4] += ALPHA * delta * f_time_to_explosion(prevWorld, character)
+        self.weights[5] += ALPHA * delta * f_is_exploded(prevWorld, character)
+
+    def Q(self, world, character):
+        sum = 0
+        sum += self.weights[0] * f_to_exit(world, character)
+        sum += self.weights[1] * f_to_monster(world, character)
+        sum += self.weights[2] * f_to_bomb(world, character)
+        sum += self.weights[3] * f_to_wall(world, character)
+        sum += self.weights[4] * f_time_to_explosion(world, character)
+        sum += self.weights[5] * f_is_exploded(world, character)
+
+        return sum
