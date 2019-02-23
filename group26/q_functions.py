f19bf6d484d436ddd45b20cfd4e9a41cd94b664b
samgoldman <sgoldman216@gmail.com>
2019-02-23 11:41:08 -0500

    Added q_functions stub

---
+++ group26/q_functions.py
@@ -0,0 +1,33 @@
+import math
+from world_utilities import *
+
+
+# 1/(distance to closest exit)^2
+def f_to_exit(world, character):
+    pass
+
+
+# 1/(distance to closest monster)^2
+def f_to_monster(world, character):
+    pass
+
+
+# 1/(distance to closest bomb)^2
+def f_to_bomb(world, character):
+    pass
+
+
+# 1/(distance to closest wall)^2
+def f_to_wall(world, character):
+    pass
+
+
+# 1/(time to explosion of closest bomb)^2
+def f_time_to_explosion(world, character):
+    pass
+
+
+# return 1 if the cell the character is in will explode, or has exploded
+# return 0 if the cell the character is in will not explode
+def f_is_exploded(world, character):
+    pass
